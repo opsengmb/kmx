eb1fc63169c4e5f14d8944dd67b2dca251de4117
Ops Eng <opsengmb@gmail.com>
2024-12-07 23:20:36 +0800

    corrected oss name

--- terraform/alicloud/backend.tf
+++ terraform/alicloud/backend.tf
@@ -1,7 +1,7 @@
 // alibaba backend configuration for terraform
 terraform {
   backend "oss" {
-    bucket = "dynamic_env-state-bucket-kmx"
+    bucket = "dynamic_env-state-bucket-kmxx"
     key   = "dynamic_env-state.tfstate"
     region = "ap-southeast-1"
   }

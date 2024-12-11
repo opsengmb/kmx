8fee9dc5447a5156e3fdd098996d11a30825e38b
Ops Eng <opsengmb@gmail.com>
2024-12-11 21:44:52 +0800

    add kumaxx bacca

---
+++ terraform/alicloud/dev-resources-bacca.tf
@@ -0,0 +1,134 @@
+resource "alicloud_slb_load_balancer" "clb-bacca" {
+  count = var.env_name == "dev" ? 1 : 0
+  load_balancer_name = "${var.env_name}-${var.project}-clb-bacca"
+  address_type       = "internet"
+  load_balancer_spec = "slb.s2.small"
+  vswitch_id         = module.vpc.vpc_id
+  tags = {
+    info = "create for internet"
+  }
+  instance_charge_type = "PayBySpec"
+}
+
+resource "alicloud_slb_listener" "listener-bacca" {
+  count = var.env_name == "dev" ? 1 : 0
+  load_balancer_id          = alicloud_slb_load_balancer.clb-bacca[count.index].id
+  server_group_id           = alicloud_slb_server_group.default-bacca[count.index].id
+  backend_port              = 8080
+  frontend_port             = 8080
+  protocol                  = "tcp"
+  bandwidth                 = -1
+  cookie_timeout            = 86400
+  cookie                    = "tfslblistenercookie"
+  health_check              = "on"
+  health_check_type         = "tcp"
+  health_check_connect_port = 8080
+  unhealthy_threshold       = 8
+  health_check_timeout      = 8
+  health_check_interval     = 5
+  health_check_http_code    = "http_2xx,http_3xx"
+  x_forwarded_for {
+    retrive_slb_ip = true
+    retrive_slb_id = true
+  }
+  acl_status      = "on"
+  acl_type        = "white"
+  acl_id          = alicloud_slb_acl.acl-bacca[count.index].id
+  request_timeout = 80
+  idle_timeout    = 30
+}
+
+resource "alicloud_slb_listener" "http-listener-bacca" {
+  count = var.env_name == "dev" ? 1 : 0
+  load_balancer_id          = alicloud_slb_load_balancer.clb-bacca[count.index].id
+  server_group_id           = alicloud_slb_server_group.http-default-bacca[count.index].id
+  backend_port              = 80
+  frontend_port             = 80
+  protocol                  = "http"
+  bandwidth                 = -1
+  sticky_session            = "on"
+  sticky_session_type       = "insert"
+  cookie_timeout            = 86400
+  cookie                    = "tfslblistenercookie"
+  health_check              = "on"
+  health_check_connect_port = 80
+  health_check_type         = "http"
+  unhealthy_threshold       = 8
+  health_check_timeout      = 8
+  health_check_interval     = 5
+  health_check_http_code    = "http_2xx,http_3xx"
+  x_forwarded_for {
+    retrive_slb_ip = true
+    retrive_slb_id = true
+  }
+  acl_status      = "on"
+  acl_type        = "white"
+  acl_id          = alicloud_slb_acl.acl-bacca[count.index].id
+  request_timeout = 80
+  idle_timeout    = 30
+}
+
+
+
+resource "alicloud_slb_acl" "acl-bacca" {
+  count = var.env_name == "dev" ? 1 : 0
+  name       = "${var.env_name}-${var.project}-clb-acl-bacca"
+  ip_version = "ipv4"
+}
+
+/*
+resource "alicloud_slb_acl_entry_attachment" "allow-all" {
+  count = var.env_name == "dev" ? 1 : 0
+  acl_id  = alicloud_slb_acl.acl[count.index].id
+  entry   = "0.0.0.0/0"
+  comment = "Allow all"
+}
+*/
+
+// listener tcp
+resource "alicloud_slb_server_group" "default-bacca" {
+  count = var.env_name == "dev" ? 1 : 0
+  load_balancer_id = alicloud_slb_load_balancer.clb-bacca[count.index].id
+  name             = "${var.env_name}-${var.project}-clb-bacca-server-attachment-tcp"
+}
+
+resource "alicloud_slb_server_group_server_attachment" "server_attachment-bacca" {
+  count = var.env_name == "dev" ? 1 : 0
+  server_group_id = alicloud_slb_server_group.default-bacca[count.index].id
+  server_id       = alicloud_instance.dev_ecs_instance_1-bacca[count.index].id
+  port            = 8080
+  weight          = 100
+}
+
+// listner http
+resource "alicloud_slb_server_group" "http-default-bacca" {
+  count = var.env_name == "dev" ? 1 : 0
+  load_balancer_id = alicloud_slb_load_balancer.clb-bacca[count.index].id
+  name             = "${var.env_name}-${var.project}-clb-bacca-server-attachment"
+}
+
+resource "alicloud_slb_server_group_server_attachment" "http_server_attachment-bacca" {
+  count = var.env_name == "dev" ? 1 : 0
+  server_group_id = alicloud_slb_server_group.http-default-bacca[count.index].id
+  server_id       = alicloud_instance.dev_ecs_instance_1-bacca[count.index].id
+  port            = 80
+  weight          = 100
+}
+
+
+resource "alicloud_instance" "dev_ecs_instance_1-bacca" {
+  count = var.env_name == "dev" ? 1 : 0
+  resource_group_id    = alicloud_resource_manager_resource_group.rg.id 
+  instance_name        = "${var.env_name}-${var.project}-dev-test-bacca"
+  image_id             = "ubuntu_24_04_x64_20G_alibase_20240923.vhd"
+  instance_type        = "ecs.g7.large"
+  security_groups      = [alicloud_security_group.dev-sg[count.index].id]
+  vswitch_id           = module.vpc.vswitch_ids[1]
+  password             = "dynamic_random_password"
+  system_disk_category = "cloud_essd"
+  system_disk_size     = 100
+  tags = {
+    Name = "${var.env_name}-${var.project}-dev-test-bacca"
+  }
+
+}
